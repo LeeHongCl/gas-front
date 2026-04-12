713d0b934cc155460a24abe4f3a5237bea4a78d5
LeeHongCl <dlghdcjf76@naver.com>
2026-04-12 15:37:07 +0900

    ㅇㅇ

--- src/api/gasStationApi.ts
+++ src/api/gasStationApi.ts
@@ -4,7 +4,7 @@ import type {
   RadiusRecommendationRequest,
 } from '@/types/gasRecommendation';
 
-const BASE_URL = 'http://43.203.29.58:8080'
+const BASE_URL = ''
 
 
 export async function fetchRouteRecommendations(
